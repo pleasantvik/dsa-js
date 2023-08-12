e38b8ccddd72782e8f98f9701f1f7b57ef6fe75e
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-08-12 08:34:23 +0100

    binary search tree

---
+++ 016-Trees/001-binaryTree.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      if (val === current.value) return undefined;
+
+      while (true) {
+        if (val < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current?.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(val) {
+    let current = this.root;
+    let found = false;
+    if (this.root === null) return false;
+
+    while (current && !found) {
+      if (val < current.value) {
+        current = current.left;
+      } else if (val > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+}
+
+let tree = new BST();
+
+tree.insert(10);
+tree.insert(5);
+tree.insert(2);
+tree.insert(13);
+tree.insert(14);
+console.log(tree);
+
+console.log(tree.find(2));
